381e7b257e7d47fd3eabdd89799d9de34830947e
Houkessou <81317419+H-Okss@users.noreply.github.com>
2022-06-05 22:02:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,83 +11,93 @@ from collections import deque
 
 
 classes_list = ["Punch", "YoYo", "Swing", "HorseRace"]
-
-st.title("Video Classification")
-uploaded_video = st.file_uploader("Choose video", type=["mp4","mov"])
-
-writer = None
-frame_skip = 10
-
-model = load_model('model_Loss_0.18764735758304596_Accuracy_0.9420289993286133.h5')
-if uploaded_video is not None: # run only when user uploads video
-    vid = uploaded_video.name
-    with open(vid, mode='wb') as f:
-        f.write(uploaded_video.read()) # save video to disk
-
-    st.markdown(f"""
-    ### Files
-    - {vid}
-    """,
-    unsafe_allow_html=True) # display file name
-
-    vidcap = cv2.VideoCapture(vid) # load video from disk
+SEQUENCE_LENGTH = 20
+IMAGE_HEIGHT, IMAGE_WIDTH = 64,64
+model = load_model('model_Loss_0.15672941505908966_Accuracy_0.9637681245803833.h5')
 
 
-def predict_video(video_file_path, output_file_path, window_size):
+
+    # st.markdown(f"""
+    # ### Files
+    # - {vid}
+    # """,
+    # unsafe_allow_html=True) # display file name
+
+
+
+def predict_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
     
-    # Initialize a Deque Object with a fixed size which will be used to implement moving/rolling average functionality.
-    predicted_labels_probabilities_deque = deque(maxlen = window_size)
-
     # Reading the Video File using the VideoCapture Object
+
     video_reader = cv2.VideoCapture(video_file_path)
 
     # Getting the width and height of the video 
     original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
     original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # Writing the Overlayed Video Files Using the VideoWriter Object
-    video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc(*'x264'), 24, (original_video_width, original_video_height))
+    video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
+    frames_queue = deque(maxlen = SEQUENCE_LENGTH)
 
-    while True: 
+    predicted_class_name = ''
+
+    while video_reader.isOpened(): 
 
         # Reading The Frame
-        status, frame = video_reader.read() 
+        ok, frame = video_reader.read() 
 
-        if not status:
+        if not ok:
             break
 
-        resized_frame = cv2.resize(frame, (64, 64))    
+        # resized_frame = cv2.resize(frame, (64, 64))
+        resized_frame = cv2.resize(frame, (original_video_width, original_video_height))    
+    
         normalized_frame = resized_frame / 255
-        predicted_labels_probabilities = model.predict(np.expand_dims(normalized_frame, axis = 0))[0]
-        predicted_labels_probabilities_deque.append(predicted_labels_probabilities)
 
-        if len(predicted_labels_probabilities_deque) == window_size:
+        frames_queue.append(normalized_frame)
+
+        # predicted_labels_probabilities = model.predict(np.expand_dims(normalized_frame, axis = 0))[0]
+        # frames_queue.append(predicted_labels_probabilities)
+
+        if len(frames_queue) == SEQUENCE_LENGTH:
             
-            predicted_labels_probabilities_np = np.array(predicted_labels_probabilities_deque)
 
-            # Calculating Average of Predicted Labels Probabilities Column Wise 
-            predicted_labels_probabilities_averaged = predicted_labels_probabilities_np.mean(axis = 0)
+            predicted_labels_probabilities = model.predict(np.expand_dims(frames_queue, axis = 0))[0]
 
-            # Converting the predicted probabilities into labels by returning the index of the maximum value.
-            predicted_label = np.argmax(predicted_labels_probabilities_averaged)
+
+            # Get the index of class with highest probability.
+            predicted_label = np.argmax(predicted_labels_probabilities)
 
             # Accessing The Class Name using predicted label.
             predicted_class_name = classes_list[predicted_label]
           
-            # Overlaying Class Name Text Ontop of the Frame
-            cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # Write predicted class name on top of the frame.
+        cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         video_writer.write(frame)
 
-        video_reader.release()
+    video_reader.release()
     video_writer.release()
-# Setting sthe Window Size which will be used by the Rolling Average Proces
-window_size = 1
 
-output_video_file_path = 'result.mp4'
-if uploaded_video is not None:
-    predict_video(uploaded_video.name, output_video_file_path, window_size)
-    st.header("The output video")
-    st.video('result.mp4')
+def main():
+    st.title("Video Classification")
+    uploaded_video = st.file_uploader("Choose video", type=["mp4","mov"])
+
+
+    if uploaded_video is not None: # run only when user uploads video
+
+        vid = uploaded_video.name
+        vidcap = cv2.VideoCapture(vid) # load video from disk
+
+        with open(vid, mode='wb') as f:
+            f.write(uploaded_video.read()) # save video to disk
+    if st.button('Classifier la video'):    
+        output_video_file_path = 'result.mp4'
+        if uploaded_video is not None:
+            predict_video(uploaded_video.name, output_video_file_path, SEQUENCE_LENGTH)
+            st.header("The output video")
+            st.video('result.mp4')
+
+if __name__ == '__main__':
+    main()
 
     
